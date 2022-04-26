012b4034e57e0b6f1a7a05e1e50e391b188a9b86
Anderson Grotto <andersongrotto@gmail.com>
2022-04-25 22:56:23 -0300

    Commit de  Anderson Grotto. Data e Hora: 25/04/2022 - 22:56:22,92

--- Commit and Push 2.bat
+++ Commit and Push 2.bat
@@ -1,6 +1,6 @@
 git add .
 set nomeUser= Anderson Grotto
-set dataHora=%date:~0,2%/%date:~3,2%/%date:~6,4%-%time:~0,2%h
+set dataHora=%date:~0,2%/%date:~3,2%/%date:~6,4%
 git commit -m "Commit de %nomeUser%. Data e Hora: %dataHora% - %time%"
 git push -u origin main
 timeout 5
